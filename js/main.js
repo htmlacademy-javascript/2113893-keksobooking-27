6346aba259b21281bd077de774c93bb27c7c7012
Konstantin Fedorov <cos9191@mail.ru>
2022-10-23 18:00:51 +0700

    удалил фрагмент для складирования объявлений

--- js/main.js
+++ js/main.js
@@ -2,16 +2,11 @@ import {getCardsArray} from './data.js';
 import {renderElement} from './popup.js';
 import {deactivateFilters, activateFilters} from './toggle-filters.js';
 
-const fragment = document.createDocumentFragment();
 const map = document.querySelector('#map-canvas');
 const formFilters = document.querySelector('.ad-form');
 const mapFilters = document.querySelector('.map__filters');
 
-getCardsArray().forEach((value) => {
-  fragment.append(renderElement(value));
-});
-
-map.append(fragment.firstChild);
+map.append(renderElement(getCardsArray()[0]));
 
 deactivateFilters(formFilters, mapFilters);
 activateFilters(formFilters, mapFilters);
