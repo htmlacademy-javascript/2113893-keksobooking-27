e9250314af299901bdd5fb59b9ac6021f12450ff
Konstantin Fedorov <cos9191@mail.ru>
2022-10-23 14:23:59 +0700

    положил 10 карточек во фрагмент и вывел 1 карточку на карту

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@ import {getCardsArray} from './data.js';
 import {renderElement} from './popup.js';
 import {deactivateFilters, activateFilters} from './toggle-filters.js';
 
+const fragment = document.createDocumentFragment();
 const map = document.querySelector('#map-canvas');
 const formFilters = document.querySelector('.ad-form');
 const mapFilters = document.querySelector('.map__filters');
 
-map.append(renderElement(getCardsArray()[0]));
+getCardsArray().forEach((value) => {
+  fragment.append(renderElement(value));
+});
+
+map.append(fragment.firstChild);
 
 deactivateFilters(formFilters, mapFilters);
 activateFilters(formFilters, mapFilters);
-
-// getCardsArray().forEach((value) => {
-//   map.append(renderElement(value));
-// });
