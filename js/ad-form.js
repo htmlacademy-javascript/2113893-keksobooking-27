1cedf2845a0034513eaae624f1c39cb2f716c92b
Konstantin Fedorov <cos9191@mail.ru>
2022-10-28 17:44:59 +0300

    добавил синхронизацию заезда/выезда, исправил логику сообщения об ошибке числа гостей и комнат

--- js/ad-form.js
+++ js/ad-form.js
@@ -1,10 +1,15 @@
 // Модуль валидации объявления
 
-const Form = document.querySelector('.ad-form');
-const numberOfRooms = Form.querySelector('#room_number');
-const capacity = Form.querySelector('#capacity');
+const form = document.querySelector('.ad-form');
+const estatePrice = form.querySelector('#price');
+const estateTitle = form.querySelector('#title');
+const estateRooms = form.querySelector('#room_number');
+const estateCapacity = form.querySelector('#capacity');
+const timeCheckIn = form.querySelector('#timein');
+const timeCheckOut = form.querySelector('#timeout');
+const estateType = form.querySelector('#type');
 
-const pristine = new Pristine(Form, {
+const pristine = new Pristine(form, {
   classTo: 'ad-form__element',
   errorClass: 'ad-form__element--invalid',
   successClass: 'ad-form__element--valid',
@@ -13,61 +18,80 @@ const pristine = new Pristine(Form, {
   errorTextClass: 'ad-form__error-text',
 });
 
+// Проверяем поле с заголовком объявления
 const validateTitle = (value) => value.length >= 30 && value.length <= 100;
 
 pristine.addValidator(
-  Form.querySelector('#title'),
+  estateTitle,
   validateTitle,
-  'Укажите название длиной от 30 до 100 символов',
+  'Укажите заголовок длиной от 30 до 100 символов',
 );
 
+// Проверяем поле цены за ночь
 const validatePrice = (value) => value >= 0 && value <= 100000;
 
 pristine.addValidator(
-  Form.querySelector('#price'),
+  estatePrice,
   validatePrice,
   'Максимум 100 000'
 );
 
-const validateCapacityAndRooms = () => {
-  if (numberOfRooms.value === '100') {
-    return capacity.value === '0';
-  }
-  if (capacity.value === '0') {
-    return numberOfRooms.value === '100';
-  }
-  return numberOfRooms.value >= capacity.value;
+// Синхронизируем поля «Время заезда» и «Время выезда»
+const onCheckInChange = () => {
+  timeCheckOut.value = timeCheckIn.value;
 };
 
+timeCheckIn.addEventListener('change', onCheckInChange);
+
+const onCheckOutChange = () => {
+  timeCheckIn.value = timeCheckOut.value;
+};
+
+timeCheckOut.addEventListener('change', onCheckOutChange);
+
+// Добавляем влияние типа жилья на стоимость
+const onEstateTypeChange = (type) => {
+  if (type === 'bungalow') {
+    estatePrice.placeholder = 0;
+  }
+};
+
+estateType.addEventListener('change', onEstateTypeChange);
+
+// Проверяем выбор числа гостей и комнат
+const validateCapacityAndRooms = () => {
+  if (estateRooms.value === '100') {
+    return estateCapacity.value === '0';
+  }
+  if (estateCapacity.value === '0') {
+    return estateRooms.value === '100';
+  }
+  return estateRooms.value >= estateCapacity.value;
+};
+
+// Собираем сообщение об ошибке в числе комнат и/или числа гостей
 const getCapacityAndRoomsErrorMessage = () => {
-  if (numberOfRooms.value === '100') {
+  if (estateRooms.value === '100' && estateCapacity.value !== '0') {
     return 'Выберите вариант "не для гостей"';
   }
-  if (capacity.value === '0') {
+  if (estateCapacity.value === '0' && estateRooms.value !== '100') {
     return 'Выберите вариант "100 комнат"';
   }
-  return `Для ${capacity.value} гостей выберите ${capacity.value} комнаты`;
+  return `Для ${estateCapacity.value} гостей выберите ${estateCapacity.value} комнаты`;
 };
 
 pristine.addValidator(
-  capacity,
+  estateCapacity,
   validateCapacityAndRooms,
   getCapacityAndRoomsErrorMessage,
 );
 
-const onCapacityChange = () => {
-  pristine.validate();
-};
+// Добавляем проверку при изменении значений числа гостей и комнат
+estateCapacity.addEventListener('change', pristine.validate());
+estateRooms.addEventListener('change', pristine.validate());
 
-capacity.addEventListener('change', onCapacityChange);
-
-const onNumberOfRoomsChange = () => {
-  pristine.validate();
-};
-
-numberOfRooms.addEventListener('change', onNumberOfRoomsChange);
-
-Form.addEventListener('submit', (evt) => {
+// Проверяем форму вместо отправки
+form.addEventListener('submit', (evt) => {
   evt.preventDefault();
   pristine.validate();
 });
